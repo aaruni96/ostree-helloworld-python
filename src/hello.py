66734714d6d2d04042e5bdf301d927975761546d
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2023-08-10 12:06:40 +0200

    Step 3 : Swap out easy ostree binary invocations with API

--- src/hello.py
+++ src/hello.py
@@ -1,18 +1,28 @@
+import os
 import pathlib
 import subprocess
 
+import gi
+gi.require_version("OSTree", "1.0")
+
+from gi.repository import OSTree
+
+OSTREE_REPO_MODE_BARE_USER = 2
+
 pathlib.Path("ostree/tree").mkdir(parents=True,exist_ok=False)
 
-subprocess.run(["ostree", "--repo=ostree/repo", "init"], check=True)
+fd = os.open('ostree', os.O_RDONLY)
+ostrepo = OSTree.Repo.create_at(fd, "repo", OSTree.RepoMode(OSTREE_REPO_MODE_BARE_USER), None, None)
 
 with open("ostree/tree/hello.txt", 'w') as fo:
     fo.write("Hello, World!\n")
 
 subprocess.run(["ostree", "--repo=ostree/repo", "commit", "--branch=foo", "ostree/tree"], check=True)
 
-subprocess.run(["ostree", "--repo=ostree/repo", "refs"], check=True)
+status, refs = ostrepo.list_refs()
+print(list(refs.keys()))
 
-subprocess.run(["ostree", "--repo=ostree/repo", "checkout", "foo", "ostree/tree-checkout/"], check=True)
+ostrepo.checkout_at(None, fd, "tree-checkout", refs["foo"], None )
 
 with open("ostree/tree-checkout/hello.txt", 'r') as fo:
     print(fo.readline())

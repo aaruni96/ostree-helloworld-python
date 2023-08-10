c4c06af4e618de901b8127f5fab0ffbaf0014dee
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2023-08-10 16:01:22 +0200

    Step 4 : Swap out all of ostree with API
    Bonus, also add location of docs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # OSTree Hello World, in Python
 
 This is a simple example repo, to adapt the OSTree Hello World example [available here](https://ostreedev.github.io/ostree/introduction/#hello-world-example) into `python`.
+
+## Documentation
+
+OSTree docs can be found at [GI API Reference](https://lazka.github.io/pgi-docs/OSTree-1.0/index.html) and [OSTree API Reference](https://ostreedev.github.io/ostree/reference/).
--- src/hello.py
+++ src/hello.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-import subprocess
 
 import gi
 gi.require_version("OSTree", "1.0")
@@ -11,18 +10,25 @@ OSTREE_REPO_MODE_BARE_USER = 2
 
 pathlib.Path("ostree/tree").mkdir(parents=True,exist_ok=False)
 
-fd = os.open('ostree', os.O_RDONLY)
-ostrepo = OSTree.Repo.create_at(fd, "repo", OSTree.RepoMode(OSTREE_REPO_MODE_BARE_USER), None, None)
+ofd = os.open('ostree', os.O_RDONLY)
+ostrepo = OSTree.Repo.create_at(ofd, "repo", OSTree.RepoMode(OSTREE_REPO_MODE_BARE_USER), None, None)
 
 with open("ostree/tree/hello.txt", 'w') as fo:
     fo.write("Hello, World!\n")
 
-subprocess.run(["ostree", "--repo=ostree/repo", "commit", "--branch=foo", "ostree/tree"], check=True)
+ostrepo.prepare_transaction(None)
+mutree = OSTree.MutableTree.new()
+ostrepo.write_dfd_to_mtree(ofd, "tree", mutree, None, None)
+mfile = ostrepo.write_mtree(mutree, None)
+mcommit = ostrepo.write_commit(None, None, None, None, mfile[1], None)
+print(mcommit[1])
+ostrepo.transaction_set_ref(None, "foo", mcommit[1])
+ostrepo.commit_transaction(None)
 
 status, refs = ostrepo.list_refs()
 print(list(refs.keys()))
 
-ostrepo.checkout_at(None, fd, "tree-checkout", refs["foo"], None )
+ostrepo.checkout_at(None, ofd, "tree-checkout", refs["foo"], None )
 
 with open("ostree/tree-checkout/hello.txt", 'r') as fo:
     print(fo.readline())
